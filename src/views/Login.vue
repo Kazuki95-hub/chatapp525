9e5b25cce01e15c340e235276bdae0dcc4b1a558
kazuki komatsu <spitzspitzspitz14@icloud.com>
2021-05-11 09:47:45 +0900

    first-commit

---
+++ src/views/Login.vue
@@ -0,0 +1,47 @@
+<template>
+    <div>
+        <div class="header flex">
+            <div class="left">
+                <h2>Chat</h2>
+            </div>
+            <div class="right flex">
+                <p>新規登録</p>
+                <p>ログイン</p>
+            </div>
+        </div>
+        <div class="card">
+            <p>ログイン</p>
+            <div class="form">
+                <input type="text" placeholder="ID">
+                <input type="text" placeholder="パスワード">
+                <button>ログイン</button>
+            </div>
+        </div>
+    </div>
+</template>
+
+<style scoped>
+.card {
+    border:1px solid black;
+    display: inline-block;
+    width: 250px;
+}
+
+.card p{
+    text-align: center;
+}
+
+input{
+    padding: 5px;
+    margin:10px;
+}
+
+.form {
+    text-align: center;
+}
+button {
+    width: 100px;
+    margin-bottom: 30px;
+    margin-top: 30px;
+}
+</style>
---
+++ src/views/SignUp.vue
@@ -0,0 +1,40 @@
+<template>
+    <div>
+        <div class="card">
+            <p>新規登録</p>
+            <div class="form">
+                <input type="text" placeholder="ユーザーネーム">
+                <input type="text" placeholder="プロフィール">
+                <input type="text" placeholder="メールアドレス">
+                <input type="text" placeholder="パスワード">
+                <button @click= "auth()">新規登録</button>
+            </div>
+        </div>
+    </div>
+</template>
+
+<style scoped>
+.card {
+    border:1px solid black;
+    display: inline-block;
+    width: 250px;
+}
+
+.card p{
+    text-align: center;
+}
+
+input{
+    padding: 5px;
+    margin:10px;
+}
+
+.form {
+    text-align: center;
+}
+button {
+    width: 100px;
+    margin-bottom: 30px;
+    margin-top: 30px;
+}
+</style>
